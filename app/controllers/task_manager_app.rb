34a3380cbe60a24264838246d7305c9bb77f67f0
Christopher Dale <christopher.john.dale3@gmail.com>
2017-03-12 17:38:33 -0600

    Need help with saving tasks, it orinally worked, then I messed something up. I can see my original tasks

---
+++ app/controllers/task_manager_app.rb
@@ -0,0 +1,23 @@
+require_relative '../models/task.rb'
+
+class TaskManagerApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/tasks' do
+    @tasks = Task.all
+    erb :index
+  end
+
+  get '/tasks/new' do
+   erb :new
+   end
+
+  get '/tasks/:id' do
+    @task = Task.find(params[:id])
+    erb :show
+  end
+end
